cb8034b6b2c42cfc82b1a06b333ee4cd0a40ab98
Diana Vidal <dvidaal@gmail.com>
2023-02-08 16:16:50 +0100

    Add 2 tests for corner cases in HomePage component

--- src/components/HomePage/HomePage.test.tsx
+++ src/components/HomePage/HomePage.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import CharactersContextProvider from "../../store/contexts/characters/CharactersContextProvider";
 import HomePage from "./HomePage";
 
 describe("Given a HomePage component", () => {
@@ -6,7 +7,7 @@ describe("Given a HomePage component", () => {
     test("Then it should render a heading element on the screen", () => {
       render(<HomePage />);
 
-      const text = screen.getByRole("heading");
+      const text = screen.getByRole("heading", { level: 1 });
 
       expect(text).toBeInTheDocument();
     });
@@ -18,6 +19,32 @@ describe("Given a HomePage component", () => {
       const text = screen.getByText(expectedOutput);
 
       expect(text).toBeVisible();
+    });
+
+    test("Then it should show the text 'Characters'", () => {
+      const expectedResult = "Characters";
+
+      render(<HomePage />);
+
+      const result = screen.getByText("Characters");
+
+      expect(result.innerHTML).toBe(expectedResult);
+    });
+  });
+});
+
+describe("Given a Character provider", () => {
+  describe("When rendered", () => {
+    test("Then it should show the HomePage component", () => {
+      render(
+        <CharactersContextProvider>
+          <HomePage></HomePage>
+        </CharactersContextProvider>
+      );
+
+      const result = screen.getByRole("heading", { level: 1 });
+
+      expect(result).toBeInTheDocument();
     });
   });
 });
