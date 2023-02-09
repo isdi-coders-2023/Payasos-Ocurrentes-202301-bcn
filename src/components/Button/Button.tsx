fd701497487d0dacf6617bd80d2e9f95fb101e3d
Marc Girbau <marc.girbau94@gmail.com>
2023-02-09 19:26:55 +0100

    Add button to ButtonStyled and refactor Button test

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,20 +4,12 @@ interface ButtonProps {
   className: string;
   action: () => void;
   children?: JSX.Element;
-  text?: string;
 }
 
-const Button = ({
-  className,
-  action,
-  children,
-  text,
-}: ButtonProps): JSX.Element => {
+const Button = ({ className, action, children }: ButtonProps): JSX.Element => {
   return (
-    <ButtonStyled>
-      <button className={className} onClick={action}>
-        {children} {text}
-      </button>
+    <ButtonStyled className={className} onClick={action}>
+      {children}
     </ButtonStyled>
   );
 };
