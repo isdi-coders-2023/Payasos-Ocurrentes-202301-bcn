67c5611d4a294c7b68e05708112e73cc2b520849
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-02-09 20:22:53 +0100

    Test Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,32 @@ const Header = (): JSX.Element => {
   return (
     <HeaderStyled>
       <header className="header-container">
-        <span className="header-container__span">RICK AND MORTY - WIKI</span>
+        <span
+          className="header-container__span"
+          aria-label="Rick and Morty - Wiki"
+        >
+          RICK AND MORTY - WIKI
+        </span>
         <div className="header-container__navbar navbar">
-          <a href="Characters" className="navbar__anchor">
+          <a
+            href="Characters"
+            className="navbar__anchor"
+            aria-label="Characters link"
+          >
             <i className="fa-solid fa-user-group"></i>
           </a>
-          <a href="Favourites" className="navbar__anchor">
+          <a
+            href="Favourites"
+            className="navbar__anchor"
+            aria-label="Favourites link"
+          >
             <i className="fa-regular fa-heart"></i>
           </a>
-          <a href="Creativity Zone" className="navbar__anchor">
+          <a
+            href="Creativity Zone"
+            className="navbar__anchor"
+            aria-label="Creativity Zone link"
+          >
             <i className="fa-regular fa-lightbulb"></i>
           </a>
         </div>
