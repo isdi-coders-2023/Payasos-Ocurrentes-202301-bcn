5fe56c7bacc12be06c0d8b7a5c42a65cca64dfa8
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-02-10 13:03:22 +0100

    Implement Header for desktop

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,32 @@ const Header = (): JSX.Element => {
   return (
     <HeaderStyled>
       <header className="header-container">
-        <span
-          className="header-container__span"
-          aria-label="Rick and Morty - Wiki"
-        >
-          RICK AND MORTY - WIKI
-        </span>
+        <div className="header-container__title-options">
+          <span className="header-container__span">rick and morty - wiki</span>
+          <div className="header-container__desktop desktop">
+            <a
+              href="Characters"
+              className="navbar__anchor"
+              aria-label="Characters link"
+            >
+              <i className="desktop__characters">characters</i>
+            </a>
+            <a
+              href="Favourites"
+              className="navbar__anchor"
+              aria-label="Favourites link"
+            >
+              <i className="desktop__favourites">favourites</i>
+            </a>
+            <a
+              href="Creativity Zone"
+              className="navbar__anchor--creativity"
+              aria-label="Creativity Zone link"
+            >
+              <i className="desktop__creativity">creativity zone</i>
+            </a>
+          </div>
+        </div>
         <div className="header-container__navbar navbar">
           <a
             href="Characters"
--- src/components/Header/HeaderStyled.tsx
+++ src/components/Header/HeaderStyled.tsx
@@ -10,11 +10,6 @@ const HeaderStyled = styled.div`
     margin-bottom: 15px;
   }
 
-  .header-container__span {
-    color: #fff;
-    font-size: 1.6rem;
-  }
-
   .navbar {
     width: 100%;
     display: flex;
@@ -24,6 +19,45 @@ const HeaderStyled = styled.div`
 
   i {
     color: #fff;
+  }
+
+  .header-container__desktop {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    font-size: 1.8rem;
+    gap: 30px;
+    text-transform: capitalize;
+  }
+
+  .header-container__title-options {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+
+  .header-container__span {
+    color: #fff;
+    font-size: 1.6rem;
+    text-transform: uppercase;
+    width: 100%;
+    justify-content: flex-start;
+  }
+
+  .desktop__creativity {
+    white-space: nowrap;
+  }
+
+  @media (min-width: 1000px) {
+    .navbar {
+      display: none;
+    }
+  }
+
+  @media (max-width: 1000px) {
+    .header-container__desktop {
+      display: none;
+    }
   }
 `;
 
