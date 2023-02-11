9cae0514ebf12f581c24e2ddf2058fd7fe3429a2
Diana Vidal <dvidaal@gmail.com>
2023-02-11 20:47:48 +0100

    Test reducer for a single character

--- src/store/reducers/characters/characterReducer.test.ts
+++ src/store/reducers/characters/characterReducer.test.ts
@@ -1,39 +1,44 @@
-import { CharactersApiStructure } from "../../../data/types";
+import {
+  CharactersApiStructure,
+  CharacterStructure,
+} from "../../../data/types";
 import {
   CharactersAction,
   CharactersActionType,
+  LoadCharacterAction,
   LoadCharactersAction,
 } from "../../actions/characters/types";
 import charactersReducer from "./charactersReducer";
 
-describe("Given the characterReducer", () => {
+describe("Given the charactersReducer", () => {
+  const characters: CharactersApiStructure = {
+    info: { count: 0, pages: 0, next: "", prev: "" },
+    results: [
+      {
+        id: 1,
+        name: "Rick",
+        status: "",
+        species: "",
+        gender: "",
+        origin: { name: "" },
+        location: { name: "" },
+        image: "",
+      },
+      {
+        id: 2,
+        name: "Morty",
+        status: "",
+        species: "",
+        gender: "",
+        origin: { name: "" },
+        location: { name: "" },
+        image: "",
+      },
+    ],
+  };
+
   describe("When it is invoked with two characters and the loadCharacters action type", () => {
     test("Then it should return the two characters", () => {
-      const characters: CharactersApiStructure = {
-        info: { count: 0, pages: 0, next: "", prev: "" },
-        results: [
-          {
-            id: 1,
-            name: "Rick",
-            status: "",
-            species: "",
-            gender: "",
-            origin: { name: "" },
-            location: { name: "" },
-            image: "",
-          },
-          {
-            id: 2,
-            name: "Morty",
-            status: "",
-            species: "",
-            gender: "",
-            origin: { name: "" },
-            location: { name: "" },
-            image: "",
-          },
-        ],
-      };
       const expectedCharacters = characters;
       const loadAction: LoadCharactersAction = {
         type: CharactersActionType.loadCharacters,
@@ -48,31 +53,6 @@ describe("Given the characterReducer", () => {
 
   describe("When it is invoked with two characters and the defaultActtion type", () => {
     test("Then it should return the two characters", () => {
-      const characters: CharactersApiStructure = {
-        info: { count: 0, pages: 0, next: "", prev: "" },
-        results: [
-          {
-            id: 1,
-            name: "Rick",
-            status: "",
-            species: "",
-            gender: "",
-            origin: { name: "" },
-            location: { name: "" },
-            image: "",
-          },
-          {
-            id: 2,
-            name: "Morty",
-            status: "",
-            species: "",
-            gender: "",
-            origin: { name: "" },
-            location: { name: "" },
-            image: "",
-          },
-        ],
-      };
       const expectedCharacters = characters;
       const defaultAction: CharactersAction = {
         type: CharactersActionType.defaultAction,
@@ -82,6 +62,39 @@ describe("Given the characterReducer", () => {
       const returnedCharacters = charactersReducer(characters, defaultAction);
 
       expect(returnedCharacters).toStrictEqual(expectedCharacters);
+    });
+  });
+
+  describe("When it is invoked with one character and the 'loadCharacter' action type", () => {
+    test("Then it should return the single character", () => {
+      const character: CharacterStructure = {
+        id: 2,
+        name: "Morty Smith",
+        status: "",
+        species: "",
+        gender: "",
+        origin: {
+          name: "",
+        },
+        location: {
+          name: "",
+        },
+        image: "",
+      };
+
+      const loadedCharacter: CharactersApiStructure = {
+        info: { count: 0, pages: 0, next: "", prev: "" },
+        results: [character],
+      };
+
+      const loadCharacter: LoadCharacterAction = {
+        type: CharactersActionType.loadCharacter,
+        payload: character,
+      };
+
+      const returnedCharacter = charactersReducer(characters, loadCharacter);
+
+      expect(returnedCharacter).toStrictEqual(loadedCharacter);
     });
   });
 });
