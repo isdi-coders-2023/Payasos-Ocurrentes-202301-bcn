4aa4e1d123fa02e7b9311fb17f079881ca54d014
Marc Girbau <marc.girbau94@gmail.com>
2023-02-12 12:30:13 +0100

    Add pagination and next previous buttons

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,20 @@
 import HomePageStyled from "./HomePageStyled";
-import { useContext, useEffect } from "react";
+import { useCallback, useContext, useEffect } from "react";
 import CharactersList from "../../components/CharacterList/CharactersList";
 import useApi from "../../hooks/useApi/useApi";
 import CharactersContext from "../../store/contexts/characters/CharactersContext";
 import UiContext from "../../store/contexts/ui/UiContext";
+import Button from "../../components/Button/Button";
+import { CharactersApiStructure } from "../../data/types";
+import {
+  setIsLoadingActionCreator,
+  unsetIsLoadingActionCreator,
+} from "../../store/actions/ui/uiActionCreator";
+import { loadCharactersActionCreator } from "../../store/actions/characters/charactersActionCreators";
 
 const HomePage = (): JSX.Element => {
-  const { characters } = useContext(CharactersContext);
-  const { isLoading } = useContext(UiContext);
+  const { characters, dispatch } = useContext(CharactersContext);
+  const { isLoading, dispatch: uiDispatch } = useContext(UiContext);
 
   const { getCharactersApi } = useApi();
 
@@ -15,11 +22,51 @@ const HomePage = (): JSX.Element => {
     getCharactersApi();
   }, [getCharactersApi]);
 
+  const getNextPage = useCallback(async () => {
+    if (!characters.info.next) {
+      return;
+    }
+    uiDispatch(setIsLoadingActionCreator());
+
+    const response = await fetch(characters.info.next!);
+    const nextPage = (await response.json()) as CharactersApiStructure;
+
+    uiDispatch(unsetIsLoadingActionCreator());
+
+    dispatch(loadCharactersActionCreator(nextPage));
+  }, [characters.info.next, dispatch, uiDispatch]);
+
+  const getPrevPage = useCallback(async () => {
+    if (!characters.info.prev) {
+      return;
+    }
+    uiDispatch(setIsLoadingActionCreator());
+
+    const response = await fetch(characters.info.prev!);
+    const nextPage = (await response.json()) as CharactersApiStructure;
+
+    uiDispatch(unsetIsLoadingActionCreator());
+
+    dispatch(loadCharactersActionCreator(nextPage));
+  }, [characters.info.prev, dispatch, uiDispatch]);
+
   return (
     <>
       <HomePageStyled>
-        <h1>Characters</h1>
+        {!isLoading && <h1>Characters</h1>}
         {!isLoading && <CharactersList characters={characters} />}
+        <div className="button-container">
+          {!isLoading && (
+            <Button action={getPrevPage} className="button-container__prev">
+              <span>Previous</span>
+            </Button>
+          )}
+          {!isLoading && (
+            <Button action={getNextPage} className="button-container__next">
+              <span>Next</span>
+            </Button>
+          )}
+        </div>
       </HomePageStyled>
     </>
   );
--- src/pages/HomePage/HomePageStyled.tsx
+++ src/pages/HomePage/HomePageStyled.tsx
@@ -3,9 +3,22 @@ import styled from "styled-components";
 const HomePageStyled = styled.div`
   position: relative;
   color: #fff;
+
   h1 {
     padding-bottom: 20px;
     font-size: 3rem;
+  }
+
+  .button-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 20px;
+
+    button {
+      color: white;
+      font-size: 1.25rem;
+    }
   }
 `;
 
